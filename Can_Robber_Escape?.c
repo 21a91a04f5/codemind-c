6648d41b6c7030fa6f475ad14632ed209e74c77b
21a91a04f5 <98006256+21a91a04f5@users.noreply.github.com>
2023-07-07 15:37:47 +0530

    #include<stdio.h> int main() {     int n;     int x[20];     int odd=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if (x[i]%2!=0)         {             odd++;         }     }     if(odd<=2)     {         printf("YES");     }     else     {         printf("NO");     }     return 0; }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -5,18 +5,18 @@ int main()
     int x[20];
     int odd=0,i;
     scanf("%d",&n);
-    for (i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
-    scanf("%d",&x[i]);
+        scanf("%d",&x[i]);
     }
-    for (i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
         if (x[i]%2!=0)
         {
             odd++;
         }
     }
-    if (odd<=2)
+    if(odd<=2)
     {
         printf("YES");
     }
